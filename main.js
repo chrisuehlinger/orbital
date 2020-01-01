017642068b2d03ff6cf63b072feb72c87516930e
Chris Uehlinger <chris.uehlinger@gmail.com>
2020-01-01 15:44:41 -0500

    Add logic for creating and destroying windows

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, screen } = require('electron')
+const os = require('os');
+
+const HOSTNAME = os.hostname().split('.')[0];
 
 // Source: https://thecodersblog.com/play-video-unmuted-in-electron-app/
 app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required")
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
+let winStructs = [];
 
-function createWindow () {
+function init(){
+  let displays = screen.getAllDisplays();
+  for(let i = 0; i < displays.length; i++){
+    let display = displays[i];
+    console.log('DISPLAY', display);
+    if(!display.internal) {
+      createWindow(display);
+    }
+  }
+
+  screen.on('display-added', (event, newDisplay) => createWindow(newDisplay));
+  screen.on('display-removed', (event, oldDisplay) => destroyWindow(oldDisplay));
+}
+
+
+function createWindow (display) {
   // Create the browser window.
-  win = new BrowserWindow({
+  let win = new BrowserWindow({
     // width: 1920,
     // height: 1080,
     frame: false,
     fullscreen: true,
-    x: 0,
-    y: 0,
+    x: display.bounds.x,
+    y: display.bounds.y,
     autoHideMenuBar: true,
     kiosk: true,
     webSecurity: false,
-    webPreferences: {
-      nodeIntegration: true
-    }
+    backgroundColor: '#000'
   })
 
   // and load the index.html of the app.
   // win.loadFile('index.html')
-  win.loadURL('https://www.youtube.com/watch?v=GZxU_pf4p7Q')
-  setTimeout(() => {
-    win.loadFile('index.html')
-  }, 7*1000)
+  win.loadURL(`https://commodore.local/orbital.html?host=${HOSTNAME}&displayid=${display.id}`)
 
   // Open the DevTools.
   // win.webContents.openDevTools()
@@ -39,14 +53,31 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    winStructs = winStructs.filter(winstruct => winstruct.win !== win);
   })
+
+  winStructs.push({
+    win,
+    display
+  });
 }
+
+function destroyWindow(display){
+  for(let i=0; i < winStructs.length; i++) {
+    let winStruct = winStructs[i];
+    if(winStruct.display.id === display.id) {
+      winStruct.win.close();
+      break;
+    }
+  }
+}
+
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', init)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -61,7 +92,7 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    init()
   }
 })
 
